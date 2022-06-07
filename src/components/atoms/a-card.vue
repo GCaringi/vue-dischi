4ac0e22dff64866fd9a1f6a87d08de86a8199f97
GCaringi <giannicaringi94@gmail.com>
2022-06-07 18:50:00 +0200

    Fix a little bug in mobile view

--- src/components/atoms/a-card.vue
+++ src/components/atoms/a-card.vue
@@ -1,6 +1,6 @@
 <template>
     <div class = "a-card h-full flex flex-col items-center bg-[color:var(--clr-primary)] p-4">
-        <img class = "w-full aspect-square" :src= elem.poster  alt= elem.title>
+        <img class = "w-[300px] aspect-square" :src= elem.poster  alt= elem.title>
         <h2 class= "text-white font-bold text-xxl text-center" >{{elem.title}}</h2>
         <div class = "text-[color:var(--clr-tertiary)]">{{elem.author}}</div>
         <small class = "text-[color:var(--clr-tertiary)]">{{elem.year}}</small>
