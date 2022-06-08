96561d03279d2568ea517e6411959a9e6222a06f
GCaringi <giannicaringi94@gmail.com>
2022-06-08 14:53:08 +0200

    Add selet for genre

--- src/components/molecules/m-playlists.vue
+++ src/components/molecules/m-playlists.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 place-items-center py-5 gap-3">
         <card
-        v-for = "(element, index) in myData"
+        v-for = "(element, index) in getFilter"
         :key = "index"
         :elem = "element"
         />
@@ -10,14 +10,16 @@
 
 <script>
 
-import card from '../atoms/a-card.vue';
 import axios from 'axios';
+import card from '../atoms/a-card.vue';
+import sharedData from '../../shared/sharedData.js'
 
 export default {
     name: "playLists",
     data(){
         return {
             myData: [],
+            sharedData,
         }
     },
     components: {
@@ -32,8 +34,16 @@ export default {
         .catch(function(error){
             console.log(error)
         })
-
-    }
+    },
+    computed: {
+        getFilter() {
+            if (sharedData.selectOption === ""){
+                return this.myData
+            }else{
+                return this.myData.filter((elem) => elem.genre === sharedData.selectOption);
+            }
+        },
+    },
 }
 </script>
 
--- src/components/organisms/o-appHeader.vue
+++ src/components/organisms/o-appHeader.vue
@@ -1,17 +1,20 @@
 <template>
-  <header class = "bg-[color:var(--clr-primary)] h-[var(--header-height)]">
+  <header class = "bg-[color:var(--clr-primary)] h-[var(--header-height)] flex items-center justify-between px-3">
       <logo/>
+      <genre-select/>
   </header>
 </template>
 
 <script>
 
-import logo from '../atoms/a-logo.vue'
+import logo from '../atoms/a-logo.vue';
+import genreSelect from '../atoms/a-select.vue';
 
 export default {
     name : "appHeader",
     components : {
-        logo
+        logo,
+        genreSelect,
     }
 }
 </script>
